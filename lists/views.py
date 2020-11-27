4f46b64278dbd0195c12127c397e34057369165d
twinstae <rabolution@gmail.com>
2020-11-27 13:14:52 +0900

    url 정리, 테스트 정리, list.html 파일 생성

--- lists/views.py
+++ lists/views.py
@@ -7,10 +7,15 @@ def home_page(request):
     if request.method == "POST":
         new_text = request.POST.get('item_text', '')
         Item.objects.create(text=new_text)
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world')
 
     return render(
         request,
         'lists/home.html',
         {'item_list': Item.objects.all()}
     )
+
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'lists/list.html', {'item_list': items})
