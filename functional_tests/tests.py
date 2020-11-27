817d3df404caba2c13cfd3dfc8588787d2cc057f
twinstae <rabolution@gmail.com>
2020-11-27 14:07:14 +0900

    add_item to existing list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -17,16 +17,6 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
-
-        self.browser.get(self.live_server_url)
-        self.check_title()
-        item_list = ["Buy peacock feathers", "Use peacock feathers to make a fly'"]
-        self.input_items(item_list)
-        self.check_items_in_rows(item_list)
-
     def check_title(self):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -35,7 +25,7 @@ class NewVisitorTest(LiveServerTestCase):
     def input_items(self, item_list):
         for i, item in enumerate(item_list, start=1):
             self.wait.until(
-                EC.presence_of_element_located((By.ID, "id_new_item"))
+                EC.presence_of_element_located((By.ID, 'id_new_item'))
             )
             input_box = self.browser.find_element_by_id('id_new_item')
             input_box.send_keys(item)
@@ -86,4 +76,5 @@ class NewVisitorTest(LiveServerTestCase):
     def reboot_browser(self):
         self.browser.quit()
         self.browser = webdriver.Chrome()
+        self.wait = WebDriverWait(self.browser, timeout=5)
 
--- lists/views.py
+++ lists/views.py
@@ -7,8 +7,9 @@ def home_page(request):
     return render(request, 'lists/home.html')
 
 
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
     return render(request, 'lists/list.html', {'item_list': items})
 
 
@@ -16,4 +17,11 @@ def new_list(request):
     a_list = List.objects.create()
     new_text = request.POST.get('item_text', '')
     Item.objects.create(text=new_text, list=a_list)
-    return redirect('/lists/the-only-list-in-the-world')
+    return redirect(f'/lists/{a_list.id}')
+
+
+def add_item(request, list_id):
+    a_list = List.objects.get(id=list_id)
+    new_text = request.POST.get('item_text', '')
+    Item.objects.create(text=new_text, list=a_list)
+    return redirect(f'/lists/{a_list.id}')
