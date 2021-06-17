4f6303a2404c5900c4dcd71e94c6cacd9af57ee7
twinstae <rabolution@gmail.com>
2021-06-17 11:26:26 +0900

    poetry, mypy, pylint 적용. 모든 모듈과 클래스, 함수에 docstring 추가

--- cosmic/domain/batch.py
+++ cosmic/domain/batch.py
@@ -1,38 +1,56 @@
+"Batch Domain Model"
+
 from enum import Enum
-from typing import List, Optional
+from typing import List, Optional, Set
 from datetime import date
 from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
 class Line:
+    "주문 라인"
     ref: str
     sku: str
     quantity: int
 
 
 class AllocateResultCode(Enum):
+    "allocate 했을 때 결과"
     SUCCESS = "SUCCESS"
-    DIFFRENT_SKU = "Batch와 Line의 SKU가 달라요." 
+    DIFFRENT_SKU = "Batch와 Line의 SKU가 달라요."
     ALREADY_ALLOCATED_LINE = "이미 할당된 주문이에요!"
     AVAILABLE_LESS_TAHN_LINE = "재고가 부족해서 할당할 수 없어요."
 
 
 class Batch:
+    """
+    상품 Batch
+    ref : 식별자
+    sku : 가구 품목의 종류와 단위
+    quantity : 가구의 수량
+    eta : 예상 도착 날짜. 이미 stock에 들어 있으면 None
+
+    _allocate : 할당된 Line들의 식별자 Set
+    """
 
     def __init__(self, ref: str, sku: str, quantity: int, eta: Optional[date]):
         self.ref = ref
         self.sku = sku
         self.quantity = quantity
         self.eta = eta
-        self._allocate = set()
+        self._allocate: Set[str] = set()
 
-    def can_allocate(self, line):
+    def can_allocate(self, line: Line) -> AllocateResultCode:
+        """
+        이 배치가 Line에 할당(allocate)할 수 있는지 Code로 반환. 실제로 할당하지는 않는다.
+        가능한 결과는 AllocateResultCode 참조.
+        """
+
         if line.sku != self.sku:
             return AllocateResultCode.DIFFRENT_SKU
 
         if line.ref in self._allocate:
-            return AllocateResultCode.ALREADY_ALLOCATED_LINE                   
+            return AllocateResultCode.ALREADY_ALLOCATED_LINE
 
         if self.quantity < line.quantity:
             return AllocateResultCode.AVAILABLE_LESS_TAHN_LINE
@@ -40,6 +58,11 @@ class Batch:
         return AllocateResultCode.SUCCESS
 
     def allocate(self, line: Line) -> AllocateResultCode:
+        """
+        실제로 배치를 할당하고 성공, 실패 여부를 Code로 반환.
+        가능한 결과는 AllocateResultCode 참조.
+        """
+
         code = self.can_allocate(line)
 
         if code != AllocateResultCode.SUCCESS:
@@ -58,11 +81,14 @@ class Batch:
             return True
         return self.eta > other.eta
 
+    def __lt__(self, other):
+        return not self.__gt__(other)
+
 
 def allocate(line: Line, batches: List[Batch]) -> str:
+    """
+    주어진 batches 중에서 can_allocate하고 eta가 가장 빠른 값에 line을 할당하고, 해당 batch.ref를 반환.
+    """
     batch = next(b for b in sorted(batches) if b.can_allocate(line))
     batch.allocate(line)
     return batch.ref
-
-
-
--- cosmic/tests/test_batches.py
+++ cosmic/tests/test_batches.py
@@ -1,15 +1,27 @@
-from datetime import date, timedelta
+"Batch, Line 도메인 모델 테스트"
 
-from cosmic.domain.Batch import AllocateResultCode, Batch, Line, allocate
+from datetime import date, timedelta
+from cosmic.domain.batch import AllocateResultCode, Batch, Line, allocate
 
 
 def make_order_line_and_batch(sku: str, order_quantity, batch_quantity):
+    """
+    주어진 sku, order_quantity와 batch_quantity로 Line과 Batch를 만들어서 반환.
+
+    ref는 단일한 defalt 값이고, eta는 오늘(today)로 설정.
+    *주의* ref나 eta를 다르게 설정하고 싶으면 이 함수를 쓰지 말 것.
+    """
     order = Line("order-ref", sku, order_quantity)
-    batch = Batch("batch-ref", sku, batch_quantity, date.today())   
+    batch = Batch("batch-ref", sku, batch_quantity, date.today())
     return order, batch
 
 
 def test_buy_2_small_table():
+    """
+    20개가 있는 배치에서 2개 짜리 라인에 할당하면
+    SUCCESS 코드를 반환하고
+    18개가 남는다.
+    """
     order, batch = make_order_line_and_batch(
             "SMALL_TABLE",
             order_quantity=2,
@@ -22,6 +34,11 @@ def test_buy_2_small_table():
 
 
 def test_available_batch_less_than_line():
+    """
+    1개가 있는 배치에서 2개 짜리 라인에 할당하면
+    AVAILABLE_LESS_TAHN_LINE 실패 코드를 반환하고
+    그대로 1개다.
+    """
     order, batch = make_order_line_and_batch(
             "BLUE_CUSHION",
             order_quantity=2,
@@ -35,6 +52,11 @@ def test_available_batch_less_than_line():
 
 
 def test_cant_allocate_same_line_twice():
+    """
+    같은 배치에서 같은 라인에 두 번 할당하면
+    ALREADY_ALLOCATED_LINE 실패 코드를 반환하고
+    Batch에 남은 quantity 는 그대로 변하지 않는다.
+    """
     order, batch = make_order_line_and_batch("BLUE_VASE", 2, 20)
     code = batch.allocate(order)
 
@@ -48,12 +70,18 @@ def test_cant_allocate_same_line_twice():
 
 
 def test_cant_allocate_different_sku():
+    """
+    SKU 종류가 다른 라인에 할당하면
+    DIFFRENT_SKU 에러를 반환하고
+    quantity는 그대로 변하지 않는다.
+    """
     order = Line("line-4", "BLUE_VASE", 2)
     batch = Batch(ref="batch-4", sku="BLUE_CUSHION", quantity=10, eta=date.today())
 
     code = batch.allocate(order)
 
     assert code == AllocateResultCode.DIFFRENT_SKU
+    assert batch.quantity == 10
 
 
 today = date.today()
@@ -62,6 +90,10 @@ later = date.today() + timedelta(days=7)
 
 
 def test_prefers_current_stock_batches_to_shipments():
+    """
+    allocate 함수로 여러 배치 중에 하나에 할당하면
+    eta가 없는 in-stock-batch에 할당한다.
+    """
     in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
     shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
     line = Line("oref", "RETRO-CLOCK", 10)
@@ -73,6 +105,12 @@ def test_prefers_current_stock_batches_to_shipments():
 
 
 def test_prefers_earlier_batches():
+    """
+    여러 배치에 allocate 함수로 할당하는데
+    allocate 함수로 여러 배치 중에 하나에 할당하면
+    in_stock_batch가 없으면
+    가장 빠른 earliest 배치에 할당한다.
+    """
     earliest = Batch("speedy-batch", "MINIMALIST-SPOON", 100, eta=today)
     medium = Batch("normal-batch", "MINIMALIST-SPOON", 100, eta=tomorrow)
     latest = Batch("slow-batch", "MINIMALIST-SPOON", 100, eta=later)
@@ -86,8 +124,13 @@ def test_prefers_earlier_batches():
 
 
 def test_returns_allocated_batch_ref():
+    """
+    allocate 함수는 할당한 배치의 ref를 반환한다.
+    """
     in_stock_batch = Batch("in-stock-batch-ref", "HIGHBROW-POSTER", 100, eta=None)
     shipment_batch = Batch("shipment-batch-ref", "HIGHBROW-POSTER", 100, eta=tomorrow)
     line = Line("oref", "HIGHBROW-POSTER", 10)
+
     allocation = allocate(line, [in_stock_batch, shipment_batch])
+
     assert allocation == in_stock_batch.ref
